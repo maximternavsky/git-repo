3dc3e12e73ec893c667c2734e52927dcedc1c183
maximternavsky <maxim.ternavsky@gmail.com>
2017-09-21 13:23:39 +0300

    работа с java.net

--- untitled1/src/geekbrains/oop/Main.java
+++ untitled1/src/geekbrains/oop/Main.java
@@ -1,9 +1,13 @@
 package geekbrains.oop;
+import java.net.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.print("hello");
-        System.out.println(" world");
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress address[] = InetAddress.getAllByName("www.4pda.ru");
+        for (int i = 0; i < address.length; i++) {
+            System.out.println(address[i]);
+        }
+
     }
 }
