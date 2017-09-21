a1ea996d95240a2b623f172b04078032f66b9e4c
maximternavsky <maxim.ternavsky@gmail.com>
2017-09-21 13:57:14 +0300

    Демонстрация работы 43-го сокета

--- untitled1/src/geekbrains/oop/Main.java
+++ untitled1/src/geekbrains/oop/Main.java
@@ -1,12 +1,29 @@
 package geekbrains.oop;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.*;
 
 public class Main {
 
-    public static void main(String[] args) throws UnknownHostException {
-        InetAddress address[] = InetAddress.getAllByName("www.4pda.ru");
-        for (int i = 0; i < address.length; i++) {
-            System.out.println(address[i]);
+    public static void main(String[] args) {
+        int c;
+
+        try (Socket s = new Socket("whois.internic.net", 43)) {
+            InputStream in = s.getInputStream();
+            OutputStream out = s.getOutputStream();
+
+            String str = (args.length == 0 ? "Yandex.ru" : args[0]) + "\n";
+
+            byte bt[] = str.getBytes();
+
+            out.write(bt);
+
+            while((c = in.read()) != -1) {
+                System.out.print((char) c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
