ab2ac75a5c0beba68d7548fce80ffa7ea95a8832
maximternavsky <maxim.ternavsky@gmail.com>
2017-09-21 12:56:53 +0300

    добавлен вывод world

--- untitled1/src/geekbrains/oop/Main.java
+++ untitled1/src/geekbrains/oop/Main.java
@@ -3,6 +3,7 @@ package geekbrains.oop;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.print("hello");
+        System.out.println(" world");
     }
 }
