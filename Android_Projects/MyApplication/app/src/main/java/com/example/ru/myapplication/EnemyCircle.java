c5c21596e81c270064c5e795cf1c417f7c266395
maximternavsky <maxim.ternavsky@gmail.com>
2017-09-25 19:52:25 +0300

    Добавлено движение вражеских кругов

--- Android_Projects/MyApplication/app/src/main/java/com/example/ru/myapplication/EnemyCircle.java
+++ Android_Projects/MyApplication/app/src/main/java/com/example/ru/myapplication/EnemyCircle.java
@@ -8,21 +8,28 @@ import java.util.Random;
  * Created by Максим on 25.09.2017.
  */
 public class EnemyCircle extends SimpleCircle {
+    public static final int RANDOM_SPEED = 10;
     public static final int FROM_RADIUS = 10;
     public static final int TO_RADIUS = 110;
     public static final int ENEMY_COLOR = Color.RED;
     public static final int FOOD_COLOR = Color.rgb(0, 200, 0);
+    private int dx;
+    private int dy;
 
-    public EnemyCircle(int x, int y, int radius) {
+    public EnemyCircle(int x, int y, int radius, int dx, int dy) {
         super(x, y, radius);
+        this.dx = dx;
+        this.dy = dy;
     }
 
     public static EnemyCircle getRandomCircle() {
         Random random = new Random();
         int x = random.nextInt(GameManager.getWidth());
         int y = random.nextInt(GameManager.getHeight());
+        int dx = 1 + random.nextInt(RANDOM_SPEED);
+        int dy = 1 + random.nextInt(RANDOM_SPEED);
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
-        EnemyCircle enemyCircle = new EnemyCircle(x, y, radius);
+        EnemyCircle enemyCircle = new EnemyCircle(x, y, radius, dx, dy);
         enemyCircle.setColor(ENEMY_COLOR);
         return enemyCircle;
     }
@@ -40,5 +47,10 @@ public class EnemyCircle extends SimpleCircle {
             return true;
         }
         return false;
+    }
+
+    public void moveOneStep() {
+        x += dx;
+        y += dy;
     }
 }
--- Android_Projects/MyApplication/app/src/main/java/com/example/ru/myapplication/GameManager.java
+++ Android_Projects/MyApplication/app/src/main/java/com/example/ru/myapplication/GameManager.java
@@ -64,5 +64,12 @@ public class GameManager {
 
     public void onTouchEvent(int x, int y) {
         mainCircle.moveMainCircleWhenTouchAt(x, y);
+        moveCircles();
+    }
+
+    private void moveCircles() {
+        for (EnemyCircle circle : circles) {
+            circle.moveOneStep();
+        }
     }
 }
