a394b4e14d805b75e0fd159d9a199deb7f38a2c5
maximternavsky <maxim.ternavsky@gmail.com>
2017-09-25 18:54:01 +0300

    Создана безопасная зона для игрока

--- Android_Projects/MyApplication/app/src/main/java/com/example/ru/myapplication/EnemyCircle.java
+++ Android_Projects/MyApplication/app/src/main/java/com/example/ru/myapplication/EnemyCircle.java
@@ -1,5 +1,7 @@
 package com.example.ru.myapplication;
 
+import android.graphics.Color;
+
 import java.util.Random;
 
 /**
@@ -8,6 +10,8 @@ import java.util.Random;
 public class EnemyCircle extends SimpleCircle {
     public static final int FROM_RADIUS = 10;
     public static final int TO_RADIUS = 110;
+    public static final int ENEMY_COLOR = Color.RED;
+    public static final int FOOD_COLOR = Color.rgb(0, 200, 0);
 
     public EnemyCircle(int x, int y, int radius) {
         super(x, y, radius);
@@ -19,6 +23,22 @@ public class EnemyCircle extends SimpleCircle {
         int y = random.nextInt(GameManager.getHeight());
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
         EnemyCircle enemyCircle = new EnemyCircle(x, y, radius);
+        enemyCircle.setColor(ENEMY_COLOR);
         return enemyCircle;
+    }
+
+    public void setEnemyOrFoodColorDependsOn(MainCircle mainCircle) {
+        if (isSmallerThan(mainCircle)){
+            setColor(FOOD_COLOR);
+        } else {
+            setColor(ENEMY_COLOR);
+        }
+    }
+
+    private boolean isSmallerThan(SimpleCircle circle) {
+        if (radius < circle.radius) {
+            return true;
+        }
+        return false;
     }
 }
--- Android_Projects/MyApplication/app/src/main/java/com/example/ru/myapplication/MainCircle.java
+++ Android_Projects/MyApplication/app/src/main/java/com/example/ru/myapplication/MainCircle.java
@@ -1,14 +1,18 @@
 package com.example.ru.myapplication;
 
+import android.graphics.Color;
+
 /**
  * Created by Максим on 25.09.2017.
  */
 public class MainCircle extends SimpleCircle{
     public static final int INIT_RADIUS = 50;
     public static final int MAIN_SPEED = 30;
+    public static final int OUR_COLOR = Color.BLUE;
 
     public MainCircle(int x, int y) {
         super(x, y, INIT_RADIUS);
+        setColor(OUR_COLOR);
     }
 
     public void moveMainCircleWhenTouchAt(int x1, int y1) {
