a2d7dba9fbc830322b1233abccc3098388675d4d
maximternavsky <maxim.ternavsky@gmail.com>
2017-09-30 11:12:44 +0300

    Добавлено второе Activity

--- Android_Projects/MyLessonApplication/app/src/main/java/com/maximternavsky/myapplication/MainActivity.java
+++ Android_Projects/MyLessonApplication/app/src/main/java/com/maximternavsky/myapplication/MainActivity.java
@@ -3,8 +3,10 @@ package com.maximternavsky.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonListener(View view) {
-
+        this.i++;
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText("Hello!" + this.i);
     }
 }
