628935093b4cb8b9046d48877ef6e03e37577336
maximternavsky <maxim.ternavsky@gmail.com>
2017-10-03 22:30:23 +0300

    Изучен обработчик нажатий кнопки

--- Android_Projects/MyLessonApplication/app/src/main/java/com/maximternavsky/myapplication/MainActivity.java
+++ Android_Projects/MyLessonApplication/app/src/main/java/com/maximternavsky/myapplication/MainActivity.java
@@ -1,22 +1,50 @@
 package com.maximternavsky.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private int i = 0;
+    private TextView textView;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    String string = "asdfghj, dsfgh, aaaa";
+    String[] split;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void buttonListener(View view) {
-        this.i++;
-        TextView textView = (TextView) findViewById(R.id.textView2);
-        textView.setText("Hello!" + this.i);
+        textView = (TextView) findViewById(R.id.textView2);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        split = string.split(", ");
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.button1: textView.setText(split[0]);
+                        break;
+                    case R.id.button2: textView.setText(split[1]);
+                        break;
+                    case R.id.button3: textView.setText(split[2]);
+                        break;
+                }
+            }
+        };
+
+        button1.setOnClickListener(onClickListener);
+        button2.setOnClickListener(onClickListener);
+        button3.setOnClickListener(onClickListener);
+
     }
 }
