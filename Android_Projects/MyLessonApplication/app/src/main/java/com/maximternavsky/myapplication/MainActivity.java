50d0985512a02bee1471f8ba6af6b5c7b16177be
maximternavsky <maxim.ternavsky@gmail.com>
2017-10-04 17:32:48 +0300

    Добавлено разрешение работы с сетью Android и отправка и прием POST запросов

--- Android_Projects/MyLessonApplication/app/src/main/java/com/maximternavsky/myapplication/MainActivity.java
+++ Android_Projects/MyLessonApplication/app/src/main/java/com/maximternavsky/myapplication/MainActivity.java
@@ -1,50 +1,111 @@
 package com.maximternavsky.myapplication;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity {
-    private int i = 0;
     private TextView textView;
+    private EditText editText;
     private Button button1;
-    private Button button2;
-    private Button button3;
-    String string = "asdfghj, dsfgh, aaaa";
+    private String string = "hello world";
     String[] split;
+    String p;
+    MyTask myTask;
+
+    public void setString (String str) {
+        string = str;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView2);
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
         button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
 
         split = string.split(", ");
+
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 switch (view.getId()) {
-                    case R.id.button1: textView.setText(split[0]);
-                        break;
-                    case R.id.button2: textView.setText(split[1]);
-                        break;
-                    case R.id.button3: textView.setText(split[2]);
+                    case R.id.button1:
+                        myTask = new MyTask();
+                        myTask.execute();
                         break;
                 }
             }
         };
 
-        button1.setOnClickListener(onClickListener);
-        button2.setOnClickListener(onClickListener);
-        button3.setOnClickListener(onClickListener);
 
+        button1.setOnClickListener(onClickListener);
     }
+
+
+
+    public class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            textView.setText("Begin");
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            String line = null;
+
+            try {
+                // Construct data
+                String data = URLEncoder.encode("lang", "UTF-8") + "=" + URLEncoder.encode("en-ru", "UTF-8");
+                data += "&" + URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode("trnsl.1.1.20" +
+                        "171003T125746Z.d9d155d60493b199.680f5b1d70acf9618012a2f7c31f40cee1cce4f6", "UTF-8");
+                data += "&" + URLEncoder.encode("text", "UTF-8") + "=" + URLEncoder.encode(string, "UTF-8");
+                // Send data
+                URL url = new URL("https://translate.yandex.net/api/v1.5/tr.json/translate?");
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write(data);
+                wr.flush();
+
+                // Get the response
+                BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line = rd.readLine()) != null) {
+                    p = line;
+                }
+                wr.close();
+                rd.close();
+            } catch (Exception e) {
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            textView.setText(p);
+        }
+    }
+
+
 }
