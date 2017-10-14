9362e5c67ea932ea85643c20a0e6cfebc05ef641
maximternavsky <maxim.ternavsky@gmail.com>
2017-10-14 14:02:30 +0300

    Создано приложение переводчик с переработанным дизайном

---
+++ Android_Projects/MyTranslator/app/src/main/java/com/maximternavsky/mytranslator/PostRequest.java
@@ -0,0 +1,83 @@
+package com.maximternavsky.mytranslator;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+public class PostRequest extends AsyncTask<Void, Void, Void> {
+
+    public static final String COPYRIGHT = "\n\nПереведено с помощью Яндекс.";
+    private final String LANG_RU_ENG = "ru-en";
+    private final String LANG_ENG_RU = "en-ru";
+
+    private String lang;
+    private String[] arrString;
+
+    private MainActivity mainActivity;
+
+
+
+    PostRequest(MainActivity mainActivity) {
+        this.mainActivity = (MainActivity) mainActivity;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+
+        mainActivity.textViewsetText("translate...");
+
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        String line = null;
+
+        int sel = mainActivity.getSelectSpinner();
+        switch (sel) {
+            case 0: lang = LANG_RU_ENG;
+                break;
+            case 1: lang = LANG_ENG_RU;
+                break;
+        }
+
+        try {
+            // Construct data
+            String data = URLEncoder.encode("lang", "UTF-8") + "=" + URLEncoder.encode(lang, "UTF-8");
+            data += "&" + URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode("trnsl.1.1.20" +
+                    "171003T125746Z.d9d155d60493b199.680f5b1d70acf9618012a2f7c31f40cee1cce4f6", "UTF-8");
+            data += "&" + URLEncoder.encode("text", "UTF-8") + "=" + URLEncoder.encode(mainActivity.getString(), "UTF-8");
+            // Send data
+            URL url = new URL("https://translate.yandex.net/api/v1.5/tr.json/translate?");
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(data);
+            wr.flush();
+
+            // Get the response
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            while ((line = rd.readLine()) != null) {
+                arrString = line.split("\\[\"");
+                arrString = arrString[1].split("\"\\]");
+                mainActivity.setLangString(arrString[0]);
+            }
+            wr.close();
+            rd.close();
+        } catch (Exception e) {
+            mainActivity.setLangString("No internet connection.");
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        mainActivity.textViewsetText(mainActivity.getLangString() + COPYRIGHT);
+    }
+}
