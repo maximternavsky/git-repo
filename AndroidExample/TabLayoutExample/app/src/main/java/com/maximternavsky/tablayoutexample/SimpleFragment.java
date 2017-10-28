79bf2d847060233748856a380d224169cc431aa5
maximternavsky <maxim.ternavsky@gmail.com>
2017-10-28 21:43:50 +0300

    Создан пример

---
+++ AndroidExample/TabLayoutExample/app/src/main/java/com/maximternavsky/tablayoutexample/SimpleFragment.java
@@ -0,0 +1,22 @@
+package com.maximternavsky.tablayoutexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SimpleFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_simple_fragment, container, false);
+    }
+
+}
